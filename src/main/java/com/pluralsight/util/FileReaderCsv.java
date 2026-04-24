c73c6d6c9d5e85ac6d391d9018ac5f69794c722b
aungkyawmyint <aungkyawmyint1096@gmail.com>
2026-04-24 15:27:28 -0700

    adding Readme file

--- src/main/java/com/pluralsight/util/FileReaderCsv.java
+++ src/main/java/com/pluralsight/util/FileReaderCsv.java
@@ -1,4 +1,37 @@
 package com.pluralsight.util;
 
-public class FileReader {
+import java.io.File;
+
+public class FileReaderCsv {
+
+    public void fileReading(File file){
+        /*
+        try {
+            File file = new File("/C:/pluralsight/Puralsight-Modules/week3/bedtime-stories/src/main/resources/payroll.csv");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Employee employee= new Employee();
+
+                String[] parts = line.split("\\|");
+
+                employee.setEmployeeId(Integer.parseInt(parts[0]));
+                employee.setName(parts[1]);
+                employee.setHoursWorked(Double.parseDouble(parts[2]));
+                employee.setPayRate(Double.parseDouble(parts[3]));
+
+                employees.add(employee);
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return employees;
+
+         */
+    }
 }
--- src/main/java/com/pluralsight/view/StarterView.java
+++ src/main/java/com/pluralsight/view/StarterView.java
@@ -1,4 +1,121 @@
 package com.pluralsight.view;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class StarterView {
+
+    public void startApplication(){
+
+        boolean flag= true;
+        Scanner sc= new Scanner(System.in);
+
+        while (true){
+            mainMenu();
+            System.out.print("Please choose the menu :");
+            String option= sc.nextLine();
+
+            switch (option) {
+                case "D":
+
+                    break;
+                case "P":
+
+                    break;
+                case "L":
+
+                    break;
+                case "E":
+                    flag=false;
+                    break;
+                default:
+                    System.out.println("Invalid Option!");
+            }
+        }
+    }
+
+    public void ledgerFunction(Scanner sc){
+        ledgerMenu();
+        System.out.print("Please choose the ledger option :");
+        String option= sc.nextLine();
+
+        switch (option) {
+            case "A":
+
+                break;
+            case "D":
+
+                break;
+            case "P":
+
+                break;
+            case "R":
+                reports(sc);
+                break;
+            case "H":
+                break;
+            default:
+                System.out.println("Invalid Option!");
+        }
+    }
+
+    public void reports(Scanner sc){
+        reportsMenu();
+        System.out.print("Please choose the reports list :");
+        int option= sc.nextInt();
+
+        switch (option) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+
+            case 0:
+                ledgerFunction(sc);
+                break;
+            default:
+                System.out.println("Invalid Option!");
+                ledgerFunction(sc);
+        }
+
+    }
+
+    public void mainMenu(){
+        System.out.println("D) Add Deposit.");
+        System.out.println("P) Make Payment [Debit].");
+        System.out.println("L) Ledger.");
+        System.out.println("E) Exit.");
+    }
+
+    public void ledgerMenu(){
+        System.out.println("A) All.");
+        System.out.println("D) Deposits.");
+        System.out.println("P) Payments.");
+        System.out.println("R) Reports.");
+        System.out.println("H) Home.");
+    }
+
+    public void reportsMenu(){
+        System.out.println("1) Month To Date.");
+        System.out.println("2) Previous Month.");
+        System.out.println("3) Year To Date.");
+        System.out.println("4) Previous Year.");
+        System.out.println("5) Search by Vendor.");
+        System.out.println("6) Custom Search.");
+        System.out.println("0) Back.");
+    }
+
+
 }
